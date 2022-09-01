1f54fa05084233c36489b2c12fc65e8894a60a1e
isachomonik <dev.isachomonik@gmail.com>
2022-09-01 10:17:57 -0300

    Função de busca pelo nome

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -5,18 +5,27 @@ const pizzas = require('../database/pizzas.json');
 module.exports = {
 
     index: (req, res) => {
-        res.render('index.ejs', {cardapio: pizzas});
+        res.render('index.ejs',{ pizzas });
     },
+
     show: (req, res) => {
-        let {id} = req.params
-        let pizza = pizzas.find(p => p.id == id);
-        // res.send(pizza)
+        // Levantar o id que veio no parâmetro de rota
+        let id = req.params.id;
 
-        res.render('pizza.ejs', {pizza})
+        // Encontrar no array de pizzas a pizza
+        let pizza = pizzas.find(p=>p.id == id);
 
-        
+        // Retornar a view pizza.ejs, a pizza encontrada
+        res.render('pizza.ejs',{pizza});
     },
+
     search: (req, res) => {
+        // Levantar o trecho que está sendo buscado (req.query.q)
+        let termoBuscado = req.query.q;
+        // Filtrar as pizzas para obter somente as pizzas com esse trecho
+        let pizzasFiltradas = pizzas.filter(p => p.nome.toLowerCase().includes(termoBuscado.toLowerCase()))
+        // retornar a view index.ejs, passando para ela somente as pizzas filtradas
+        res.render('index.ejs', { pizzas: pizzasFiltradas });
     }
 
 }
